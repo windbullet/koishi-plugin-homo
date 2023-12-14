a4a870d35f572448a698d3d4c8bc9dd976690a76
windbullet <2536810643@qq.com>
2023-12-14 11:47:26 +0800

    提交文件

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
-# koishi-plugin-xiaixue
+# koishi-plugin-homo
 
-[![npm](https://img.shields.io/npm/v/koishi-plugin-xiaixue?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-xiaixue)
+[![npm](https://img.shields.io/npm/v/koishi-plugin-homo?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-homo)
 
-ok
+恶臭数字论证器
+
+引用：https://github.com/itorr/homo
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Context, Schema } from 'koishi'
 
-export const name = 'xiaixue'
+import homo from './homo.js'
+
+export const name = 'homo'
 
 export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('恶臭论证 <num:number>', '恶臭数字论证器', {checkArgCount: true})
+    .example("恶臭论证 1919810")
+    .action(async ({ session }, num) => {
+      session.send(`${num} = ${homo(num)}`)
+    })
 }
